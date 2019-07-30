2fc283e8c12801276238e5c7ca5432e7a1504343
ziwei <18500905537@163.com>
2019-07-30 11:18:32 +0800

    不修改HomeTest

--- src/test/java/com/github/hcsp/HomeTest.java
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -7,7 +7,7 @@ public class HomeTest {
     @Test
     public void referencePassingTest() {
         Assertions.assertEquals(null, Home.cat);
-        Home.newCat(Home.cat);
+        Home.newCat();
         Assertions.assertEquals("Cat(Tom)", Home.cat.toString());
     }
 }
