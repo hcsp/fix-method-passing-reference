f4ac96f3323428adb813810d9b304816ffc864c6
RookieZ <guilongzhongwei@hotmail.com>
2019-09-14 16:59:48 +0800

    Update HomeTest.java

--- src/test/java/com/github/hcsp/HomeTest.java
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -1,13 +1,13 @@
-//package com.github.hcsp;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//public class HomeTest {
-//    @Test
-//    public void referencePassingTest() {
-//        Assertions.assertEquals(null, Home.cat);
-//        Home.newCat();
-//        Assertions.assertEquals("Cat(Tom)", Home.cat.toString());
-//    }
-//}
+package com.github.hcsp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HomeTest {
+    @Test
+    public void referencePassingTest() {
+        Assertions.assertEquals(null, Home.cat);
+        Home.newCat();
+        Assertions.assertEquals("Cat(Tom)", Home.cat.toString());
+    }
+}
