1af788f86f4205597f049005cfa227418f59702e
Ditto0 <46198155+Ditto0@users.noreply.github.com>
2019-10-20 18:05:58 +0800

    8

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -6,7 +6,7 @@ public class Cat {
     public Cat(String name) {
         this.name = name;
     }
-
+    public  Cat(){}
     @Override
     public String toString() {
         return "Cat(" + name + ")";
