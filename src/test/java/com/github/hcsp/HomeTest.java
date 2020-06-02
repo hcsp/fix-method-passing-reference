46ef7dc806b90840fde24d17aecd12563fb72e22
kkeda <kkeda725@gmail.com>
2020-06-02 20:17:18 +0800

    修复新创建一只Cat 但是没有生效 参数引用问题

--- src/test/java/com/github/hcsp/HomeTest.java
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -3,8 +3,7 @@ package com.github.hcsp;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
-class HomeTest {
+public class HomeTest {
     @Test
     public void referencePassingTest() {
         Assertions.assertEquals(null, Home.cat);
