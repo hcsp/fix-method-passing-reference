e0282403b7c5a56a4e12660702f33d3a17cd8a6a
StudyJavaLyw <79556328+StudyJavaLyw@users.noreply.github.com>
2021-03-29 16:14:25 +0800

    1 (#259)
    1 (#259)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -3,18 +3,18 @@ package com.github.hcsp;
 import com.github.hcsp.pet.Cat;
 
 public class Home {
-    public static Cat cat;
+  public static Cat cat;
 
-    public static void main(String[] args) {
-        // We want to create a new Cat, but it doesn't seem to be working
-        // Think why and fix the code to make the program output "Cat(Tom)"
-        // 我们想要在这里创建一只新的Cat，但是似乎没有生效
-        // 思考一下为什么并修复这个问题，使得程序输出"Cat(Tom)"
-        newCat(cat);
-        System.out.println(cat);
-    }
+  public static void main(String[] args) {
+    // We want to create a new Cat, but it doesn't seem to be working
+    // Think why and fix the code to make the program output "Cat(Tom)"
+    // 我们想要在这里创建一只新的Cat，但是似乎没有生效
+    // 思考一下为什么并修复这个问题，使得程序输出"Cat(Tom)"
+    newCat();
+    System.out.println(cat);
+  }
 
-    public static void newCat(Cat cat) {
-        cat = new Cat("Tom");
-    }
+  public static void newCat() {
+    cat = new Cat("Tom");
+  }
 }
