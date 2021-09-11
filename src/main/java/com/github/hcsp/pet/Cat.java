dc621c45cad6fee0dfdc39ae68a55d397faea83b
ROG <1049378767@qq.com>
2021-09-11 15:57:31 +0800

    修复对象引用

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -4,11 +4,13 @@ public class Cat {
     private String name;
 
     public Cat(String name) {
+
         this.name = name;
     }
 
     @Override
     public String toString() {
+
         return "Cat(" + name + ")";
     }
 }
