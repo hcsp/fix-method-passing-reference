a809e536019a9aaf538bd332290fabfa9e8ec484
yangbisheng <2606424906@qq.com>
2022-08-01 00:06:14 +0800

    fix method passing reference (#318)
    fix method passing reference (#318)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -8,13 +8,14 @@ public class Home {
     public static void main(String[] args) {
         // We want to create a new Cat, but it doesn't seem to be working
         // Think why and fix the code to make the program output "Cat(Tom)"
-        // 我们想要在这里创建一只新的Cat，但是似乎没有生效
+        // 我们想要在这里创建一只新的Cat新，但是似乎没有生效
         // 思考一下为什么并修复这个问题，使得程序输出"Cat(Tom)"
-        newCat(cat);
+        newCat();
         System.out.println(cat);
     }
 
-    public static void newCat(Cat cat) {
+    public static void newCat() {
+
         cat = new Cat("Tom");
     }
 }
