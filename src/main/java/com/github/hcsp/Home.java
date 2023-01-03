ed6174a15f1825cbe678bd935e96c31370c26100
YangYP <94595969@qq.com>
2023-01-03 18:26:38 +0800

    对象：第八题

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -10,7 +10,7 @@ public class Home {
         // Think why and fix the code to make the program output "Cat(Tom)"
         // 我们想要在这里创建一只新的Cat，但是似乎没有生效
         // 思考一下为什么并修复这个问题，使得程序输出"Cat(Tom)"
-        newCat(cat);
+        newCat();
         System.out.println(cat);
     }
 
@@ -18,9 +18,9 @@ public class Home {
        cat = new Cat("Tom");
     } */
 
-     public static void newCat(Cat cat) {
-         cat = new Cat("Tom");
-         Home.cat = cat;
+     public static void newCat() {
+          cat = new Cat("Tom");
+     //    Home.cat = cat;
     }
 
 }
